8fadc3206fbc06418c77d823b3b23a711f6d42f4
Adam Piper <adam.piper@portswigger.net>
2017-05-21 16:59:19 +0800

    Add Burp Extender metadata. Add Gradle build script.

--- burp/BurpExtender.java
+++ burp/BurpExtender.java
@@ -2,7 +2,7 @@
 Burp SniperScan Extension
 
 MIT License
-Copyright (c) 2017 Clément Notin / @cnotin
+Copyright (c) 2017 ClÃ©ment Notin / @cnotin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
